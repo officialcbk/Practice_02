afe4130dfadca28bf12ac45bc5db8968d3505953
officialcbk <eeze@academic.rrc.ca>
2024-09-22 21:39:22 -0700

    Updated Examples of List,tuples,sets

--- sdf_assignment_2.py
+++ sdf_assignment_2.py
@@ -34,10 +34,57 @@ print('Total :' , (PST * vehicle_price)+(GST * vehicle_price))
 
 
 #LISTS
+list_of_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(list_of_numbers)
+
+list_of_numbers.insert(5,"Emmanuel")
+print(list_of_numbers)
+
+list_of_numbers.remove(9)
+print(list_of_numbers)
+
+alphabets = ['A','B','C']
+
+combination_of_numbers_and_letters = list_of_numbers + alphabets
+print(combination_of_numbers_and_letters)
 
 #TUPLES
+canadian_provinces = ('MB','OT','SK','AB')
+print(canadian_provinces)  
+
+
 
 #DICTIONARIES
 
+canadian_currency = {
+    'nickel': 5,
+    'dime': 10,
+    'quarter':25,
+     'looney':100,
+     'tooney':200
+
+}
+
+print(canadian_currency)
+
+
 #SETS
+
+even_number = {2,4,6,8,10,12,14,16,18,20}
+print(even_number)
+
+multiples_of_5 = {5,10,15,20}
+print(multiples_of_5)
+
+even_numbers_multiples_of_5 ={10,20}
+print(even_numbers_multiples_of_5)
+
+common_numbers =even_number.intersection(multiples_of_5)
+print(common_numbers)
+
+even_numbers_that_are_not_multiples_of_5 = even_number.difference(multiples_of_5)
+print(even_numbers_that_are_not_multiples_of_5)
+
+multiples_of_5_that_are_not_even_numbers = multiples_of_5.difference(even_number)
+print(multiples_of_5_that_are_not_even_numbers)
 
