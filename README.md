2a39e157639422d10b73198bff8bd4c85919a1fb
officialcbk <eeze@academic.rrc.ca>
2024-09-22 19:53:01 -0700

    Showing different data types and examples

---
+++ README.md
@@ -0,0 +1,14 @@
+# [Project Name]
+Project 02
+
+## Description
+
+Module 2
+
+## Author
+
+Emmanuel Eze
+
+## Assignment
+
+Module 2 Project related to Data Types in Python
--- sdf_assignment_2.py
+++ sdf_assignment_2.py
@@ -1,6 +1,26 @@
-
+"""
+Description:Data Types In Python
+Author:Emmanuel Eze
+Date:22-9-2024
+"""
 
 #SIMPLE DATA TYPES
+name = "Emmanuel"
+print('name:' ,name)
+print('type:' ,type(name))
+
+current_driving_status = False
+print('has driving license:' , current_driving_status)
+print('type:', type(current_driving_status))
+
+current_year = 2024
+print('this year:' , current_year)
+print('type :' , type(current_year))
+
+current_year = 2024+1
+print('this year:' , current_year)
+print('type :' , type(current_year))
+
 
 #CALCULATIONS
 
